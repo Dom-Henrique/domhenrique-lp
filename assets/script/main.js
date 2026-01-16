7dd4c5176863482a3111e004d946effdee83ec59
Dom-Henrique <joaohenriqueds24@gmail.com>
2026-01-16 16:30:03 -0300

    Some images

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,8 +1,1 @@
-let images = document.querySelectorAll("img")
-
-function toggleTheme(element){
-    document.body.style.backgroundColor = "var(--primary)";
-    document.body.style.color = "var(--dark)"
-
-    images.document.style.filter = "invert(1)";
-}
+let 
