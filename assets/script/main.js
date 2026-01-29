312dc480f3be1e05792baa9f18a382c335ee7901
Dom-Henrique <joaohenriqueds24@gmail.com>
2026-01-29 13:21:33 -0300

    Player function

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,15 @@
-let 
+document.addEventListener("DOMContentLoaded", () => {
+  music = document.querySelector(".cbj");
+});
+
+
+let playing = false;
+
+function playMusic(){
+    if (!playing){
+        music.play();
+    } else{
+        music.pause()
+    }
+    playing = !playing;
+}
