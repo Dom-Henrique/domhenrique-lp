a3a8be4d728367219395e2528aab54d5303ea981
Dom-Henrique <joaohenriqueds24@gmail.com>
2026-02-12 12:27:11 -0300

    Clean code into dark mode function
    Was used the classList toggle to dark-mode class that is created and added to body when the function is activated

--- assets/script/darkMode.js
+++ assets/script/darkMode.js
@@ -1,5 +1,4 @@
 // Criação de um botão que troca o tema
-let landingPage = document.querySelector("body");
 let aboutMe = document.querySelector(".about-me");
 let darkMode = document.querySelector(".darkMode");
 let header = document.querySelector(".presentation");
@@ -8,31 +7,17 @@ let navBar = document.getElementsByTagName("header");
 let control = false;
 // Adicionar um controlador
 darkMode.addEventListener("click", function (){
-    /*
-    darkModeSymbol.classList.toggle("fa-solid fa-sun");
-    darkModeSymbol.classList.toggle("fa-solid fa-moon");
-    */
    if (control == false){
-        landingPage.style.backgroundColor = "#d0d0d0";
-        landingPage.style.color = "#202020";
-        header.style.color = "#d0d0d0"
-        aboutMe.style.backgroundColor = "#202020";
-        aboutMe.style.color = "#d0d0d0";
-        navBar[0].style.backgroundColor = "#202020";
-        navBar[0].style.color = "#d0d0d0";
-        control = true;
-        // Pega todos os elementos e muda uma propriedade em específico.
-        document.documentElement.style.setProperty('--glow', 'rgba(0, 0, 0, 0.5)');
+          document.body.classList.toggle("dark-mode");
+          control = true;
+          darkModeSymbol.classList.toggle("fa-moon");
+          // Pega todos os elementos e muda uma propriedade em específico.
+          document.documentElement.style.setProperty('--glow', 'rgba(0, 0, 0, 0.5)');
    }
    else if (control == true){
-        landingPage.style.backgroundColor = "";
-        landingPage.style.color = "";
-        header.style.color = ""
-        aboutMe.style.backgroundColor = "";
-        aboutMe.style.color = "";
-        navBar[0].style.backgroundColor = "";
-        navBar[0].style.color = "";
-        control = false;
-        document.documentElement.style.setProperty('--glow', '');
+          document.body.classList.toggle("dark-mode")
+          control = false;
+          darkModeSymbol.classList.toggle("fa-sun");
+          document.documentElement.style.setProperty('--glow', '');
    }
 })
